b2acddebd74f4f3647f0598736b8e8f634aee344
jasminisantos <jasminirebeca10@gmail.com>
2023-06-22 14:13:54 -0300

    Initial Commit

---
+++ Mini1/Mini1App.swift
@@ -0,0 +1,17 @@
+//
+//  Mini1App.swift
+//  Mini1
+//
+//  Created by Jasmini Rebecca Gomes dos Santos on 22/06/23.
+//
+
+import SwiftUI
+
+@main
+struct Mini1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
