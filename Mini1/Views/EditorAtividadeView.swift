02e6bbf73743485445fb0c64ebbfcebbd05841cc
jasminisantos <jasminirebeca10@gmail.com>
2023-07-03 21:29:23 -0300

    ajustes no editorAtividadesView

--- Mini1/Views/EditorAtividadeView.swift
+++ Mini1/Views/EditorAtividadeView.swift
@@ -8,8 +8,68 @@
 import SwiftUI
 
 struct EditorAtividadeView: View {
+    @State private var selectedTimeADay = Date()
+    @State private var selectedNotifications = Date()
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        ScrollView {
+            header
+            content
+        }
+    }
+    
+    var content: some View {
+        VStack(alignment: .leading) {
+            Text("Nome")
+                .font(.callout)
+                .bold()
+                .font(.system(size: 16))
+                .foregroundColor(.black)
+                
+            CustomTextField()
+            
+            Rectangle()
+                .frame(height: 1)
+                .opacity(0.33)
+                .foregroundColor(.gray)
+                .padding(.vertical)
+            
+            VStack(spacing: 20) {
+                Scheduler(icon: "clock.fill", color: .purple, title: "Tempo por dia", subtitle: "8:00 horas")
+                
+                CustomScheduler()
+                
+                CustomToggle2(icon: "wand.and.stars", color: .teal, title: "Adaptável")
+                    .padding(.horizontal)
+                    .background(Color(.systemGray6))
+                    .cornerRadius(8)
+                
+                Scheduler(icon: "megaphone.fill", color: .green, title: "Notificações", subtitle: "8:00 horas")
+            }
+        }
+        .padding()
+    }
+    
+    var header: some View {
+        HStack(alignment: .top, spacing: 40) {
+            Text("Cancelar")
+                .font(.body)
+                .font(.system(size: 17))
+                .foregroundColor(.blue)
+            
+            Text("Editar atividade")
+                .font(.headline)
+                .font(.system(size: 17))
+                .foregroundColor(.black)
+            
+            Text("Salvar")
+                .font(.body)
+                .font(.system(size: 17))
+                .foregroundColor(.blue)
+        }
+        .padding(.top, 30)
+        .padding(.bottom, 20)
     }
 }
 
