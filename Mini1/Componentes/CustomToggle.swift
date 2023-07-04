7b7aa2a4cb5c5e6c2f9b4891a19d42467dae0add
jasminisantos <jasminirebeca10@gmail.com>
2023-07-03 21:28:12 -0300

    implementacao customToggle

--- Mini1/Componentes/CustomToggle.swift
+++ Mini1/Componentes/CustomToggle.swift
@@ -8,13 +8,47 @@
 import SwiftUI
 
 struct CustomToggle: View {
+    @State private var isToggleOn = false
+    
+    var icon: String
+    var color: Color
+    var title: String
+    var subtitle: String
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            HStack {
+                Image(systemName: icon)
+                    .foregroundColor(.white)
+                    .frame(width: 40, height: 40)
+                    .background(color)
+                    .cornerRadius(8)
+                    .padding(.trailing, 8)
+                
+                
+                VStack(alignment: .leading, spacing: 4) {
+                    Text(title)
+                        .font(.body)
+                        .fontWeight(.regular)
+                        .foregroundColor(.black)
+                    
+                    Text(subtitle)
+                        .font(.footnote)
+                        .fontWeight(.bold)
+                        .foregroundColor(.blue)
+                }
+                
+                Spacer()
+                
+                Toggle("", isOn: $isToggleOn)
+                    .padding()
+            }
+        }
     }
 }
 
 struct CustomToggle_Previews: PreviewProvider {
     static var previews: some View {
-        CustomToggle()
+        CustomToggle(icon: "clock.fill", color: .purple, title: "Title", subtitle: "Subtitle")
     }
 }
